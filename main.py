50affdf3b78eb9491d71e8b1ca6d404e34776b33
krneki777 <theweirdguy247@gmail.com>
2018-01-23 17:12:12 +0100

    Še datoteki, ki nista novi, smo ju pa spremenil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import make_response
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +12,15 @@ def index():
 @app.route("/druga_stran")
 def druga():
     return render_template("druga_stran.html")
+
+@app.route("/blog/<int:st_blog>")
+def blog(st_blog):
+    print("Dobil sem stevilo", st_blog)
+    zadnji = request.cookies.get("blog_stevilka")
+    result = render_template("blog.html", stevilka = st_blog, prej = zadnji)
+    result = make_response(result)
+    result.set_cookie("blog_stevilka", str (st_blog))
+    return result
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
