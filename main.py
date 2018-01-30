f4765d8b0ff6dd3a52f5b5d41f83b53906ae8ef3
Maks Kolman <kolman.maks@gmail.com>
2018-01-30 15:48:48 +0100

    Dodal blog

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, make_response
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +10,15 @@ def index():
 @app.route("/druga_stran")
 def druga():
     return render_template("druga_stran.html")
+
+@app.route("/blog/<int:st_blog>")
+def blog(st_blog):
+    zadnji = request.cookies.get("blog_stevilka")
+    result = render_template(
+        "blog.html", stevilka=st_blog, prejsni=zadnji)
+    result = make_response(result)
+    result.set_cookie("blog_stevilka", str(st_blog))
+    return result
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
